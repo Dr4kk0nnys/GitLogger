df974851dbb390bc08207289e0cfc20b338909d7
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-08-18 17:32:06 -0300

    Working beta on the typescript version

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 import robotjs from 'robotjs';
-setTimeout(() => {
-    robotjs.typeString('Dr4kk0nnys');
-    robotjs.keyTap('enter');
-    robotjs.typeString('Dr4kk0nnys');
-    robotjs.keyTap('enter');
-}, 4000);
+import fs from 'fs';
+fs.readFile('database.txt', 'UTF-8', (err, data) => {
+    if (err)
+        throw err;
+    const sanitizedData = data.split('\n');
+    const [email, password] = sanitizedData;
+    setTimeout(() => {
+        robotjs.typeString(email);
+        robotjs.keyTap('enter');
+        robotjs.typeString(Buffer.from(password, 'base64').toString('ascii'));
+        robotjs.keyTap('enter');
+    }, 4000);
+});
+/*
+
+*/ 
--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 import robotjs from 'robotjs';
+import fs from 'fs';
 
 
-setTimeout(() => {
-    robotjs.typeString('Dr4kk0nnys');
-    robotjs.keyTap('enter');
+fs.readFile('database.txt', 'UTF-8', (err, data) => {
+    if (err) throw err;
 
-    robotjs.typeString('Dr4kk0nnys');
-    robotjs.keyTap('enter');
-}, 4000);
+    const [email, password] = data.split('\n');
+
+    setTimeout(() => {
+        robotjs.typeString(email);
+        robotjs.keyTap('enter');
+
+        robotjs.typeString(Buffer.from(password, 'base64').toString('ascii'));
+        robotjs.keyTap('enter');
+    }, 4000);
+});
